266906197bb5b19c7487ebf5e5c6db6ded0d2252
meproject <eyaln79@gmail.com>
2018-09-02 20:03:21 +0300

    save changes

---
+++ WebCalc/Controllers/ApiController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebCalc.Interfaces;
+using WebCalc.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebCalc.Controllers
+{
+
+    [Route("api")]
+    public class ApiController : Controller
+    {
+
+        private readonly ICalculateService _serv;
+
+        public ApiController(ICalculateService serv)
+        {
+            _serv = serv;
+        }
+
+
+        [HttpPost("calculate")]
+        public IActionResult CalcResult([FromBody]CalculatorRequest request)
+        {
+            var result = _serv.Evaluate(request.Expression);
+
+            return Json(new
+            {
+                result = result
+            });
+        }
+    }
+}
--- WebCalc/Controllers/HomeController.cs
+++ WebCalc/Controllers/HomeController.cs
@@ -23,13 +23,5 @@ namespace WebCalc.Controllers
             return View();
         }
 
-       [HttpPost("calculate")]
-        public IActionResult CalcResult([FromBody]CalculatorRequest request)
-        {
-           var result = _serv.Evaluate(request.Expression);
-            return Json(new {
-                result = result
-            });
-        }
     }
 }
