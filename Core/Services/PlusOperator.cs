6c8a24a456953633ef473b9eba52177f17b333f9
meproject <eyaln79@gmail.com>
2018-09-11 22:31:27 +0300

    add space

--- Core/Services/PlusOperator.cs
+++ Core/Services/PlusOperator.cs
@@ -10,7 +10,7 @@ namespace Core.Services
         public T Calculate<T>(T param1, T param2)
         {
             dynamic p1 = param1;
-            dynamic p2 = param2;
+            dynamic p2 = param2; 
 
             return p1 + p2;
         }
